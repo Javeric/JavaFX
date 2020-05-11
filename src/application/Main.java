3cf450754d4d6d072911fa2e22aa3288cbeacb62
Eric <ericrangel13@hotmail.com>
2020-05-11 07:52:23 -0300

    Projeto criado até o menu

---
+++ src/application/Main.java
@@ -0,0 +1,32 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			
+			Parent parent = loader.load();
+			
+			Scene mainScene = new Scene(parent);
+			
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("JavaFX application");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
